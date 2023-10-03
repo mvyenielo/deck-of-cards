06ce9a8591582db65175b4367043e5321f0ad522
Jung-GunSong <jsong781@gmail.com>
2023-10-03 15:04:25 -0400

    finsihed functionality to add cards to state

--- src/DeckApp.js
+++ src/DeckApp.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 const NEW_DECK_URL =
   "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
+const NEW_CARD_URL = "https://deckofcardsapi.com/api/deck/"
+
 function DeckApp() {
   const [deck, setDeck] = useState({ deckId: "", cards: [] });
   //state for isLoading OR conditional depending on !deckId
@@ -10,11 +12,39 @@ function DeckApp() {
   useEffect(function getNewDeck() {
     async function fetchNewDeck() {
       const response = await fetch(NEW_DECK_URL);
-      const deck = await response.json();
-      setDeck(d => ({ ...d, deckId: deck.deck_id }));
+      const cardDeck = await response.json();
+      setDeck(d => ({ ...d, deckId: cardDeck.deck_id }));
+      console.log(`Our Deck is`, deck)
     }
     fetchNewDeck();
   }, []);
+
+  function handleClick(evt){
+    evt.preventDefault();
+    drawCard();
+  }
+
+  async function drawCard(){
+    const response = await fetch(`${NEW_CARD_URL}${deck.deckId}/draw/?count=1`);
+    // console.log(`Our response is`, response);
+    const currCardData = await response.json();
+    // console.log(`Our cards are`, currCardData);
+    const currCard = currCardData.cards[0];
+    // console.log(`currCard is`, currCard);
+    setDeck( d=> ({...d, cards: [...d.cards, currCard]}));
+  }
+
+  return(
+    <div>
+      {!deck.deckId
+      ? <p>Loading!</p>
+      : <form onClick={handleClick}>
+          <button>
+            Draw a Card
+          </button>
+        </form>}
+    </div>
+  )
 
 }
 
